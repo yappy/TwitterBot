3a929904a954ee75062ae00d29bd371a2d7d0071
yappy <superunyo@yahoo.co.jp>
2011-12-02 01:49:03 +0900

    first commit.

---
+++ src/Alice.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import ec.util.MersenneTwisterFast;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+// for crontab
+// * * * * * cd ~/workspace/Twitter; /usr/java/default/bin/java -cp bin Alice
+
+public class Alice {
+
+	private static final String TWEET_FILE_NAME = "list.txt";
+
+	private static PrintWriter logOut;
+
+	private static void saySomething() {
+		try {
+			Scanner in = new Scanner(new File(TWEET_FILE_NAME), "UTF-8");
+			List<String> list = new ArrayList<String>();
+			while (in.hasNextLine()) {
+				String line = in.nextLine();
+				if (!line.equals("") && !line.startsWith("#")) {
+					list.add(line);
+				}
+			}
+			logOut.printf("List loaded (%d items)%n", list.size());
+
+			MersenneTwisterFast mt = new MersenneTwisterFast();
+			int first = mt.nextChar();
+			for (int i = 0; i < first; i++) {
+				mt.nextInt();
+			}
+			int ind = mt.nextInt(list.size());
+
+			String msg = list.get(ind);
+			Twitter twitter = new TwitterFactory().getInstance();
+			twitter.updateStatus(msg);
+			logOut.println("tweet: " + msg);
+		} catch (IOException e) {
+			e.printStackTrace(logOut);
+		} catch (TwitterException e) {
+			e.printStackTrace(logOut);
+		}
+	}
+
+	public static void main(String[] args) {
+		Date nowDate = new Date();
+		String nowStr = String.format("%1$tY%1$tm%1$td_%1$tH%1$tM%1$tS",
+				nowDate);
+
+		File logDir = new File("log");
+		logDir.mkdir();
+		String logFileName = nowStr + ".log";
+		try {
+			logOut = new PrintWriter(new File(logDir, logFileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		logOut.printf("Start %1$tF %1$tT%n", nowDate);
+
+		saySomething();
+
+		logOut.close();
+	}
+
+}
