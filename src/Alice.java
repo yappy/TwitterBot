feb1556ca23bbc39206f973b404951d3cddaac9d
yappy <superunyo@yahoo.co.jp>
2012-07-03 12:49:43 +0900

    Correct rebase.

--- src/Alice.java
+++ src/Alice.java
@@ -206,51 +206,6 @@ public class Alice {
 			}
 			for (Tweet t : hitList) {
 				System.out.println(t.getText());
-				// self tweet
-				if (t.getFromUserId() == twitter.getId())
-					continue;
-				String text = t.getText();
-				// delete mention (@xyz)
-				text = text.replaceAll("@\\w+", "");
-				for (String word : wordList) {
-					if (text.indexOf(word) != -1) {
-						hitList.add(t);
-						break;
-					}
-				}
-			}
-			for (Tweet t : hitList) {
-				System.out.println(t.getText());
-				// self tweet
-				if (t.getFromUserId() == twitter.getId())
-					continue;
-				String text = t.getText();
-				// delete mention (@xyz)
-				text = text.replaceAll("@\\w+", "");
-				for (String word : wordList) {
-					if (text.indexOf(word) != -1) {
-						hitList.add(t);
-						break;
-					}
-				}
-			}
-			for (Tweet t : hitList) {
-				System.out.println(t.getText());
-				// self tweet
-				if (t.getFromUserId() == twitter.getId())
-					continue;
-				String text = t.getText();
-				// delete mention (@xyz)
-				text = text.replaceAll("@\\w+", "");
-				for (String word : wordList) {
-					if (text.indexOf(word) != -1) {
-						hitList.add(t);
-						break;
-					}
-				}
-			}
-			for (Tweet t : hitList) {
-				System.out.println(t.getText());
 			}
 			// if (!ts.isEmpty()) {
 			// Tweet t = ts.get(0);
